f8ff9ed438619968b3cb468975f4d0d3c8e07115
Jasper Reed <jmixon.reed@gmail.com>
2020-07-11 22:30:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,25 @@
 # code goes here 
 
-def using_concat()
+def using_concat(array, array2)
+  array.concat(array2)
+end
+
+def using_insert()
+  
+end
+
+def using_uniq()
+  
+end
+
+def using_flatten()
+  
+end
+
+def using_delete()
+  
+end
+
+def using_delete_at()
+  
+end
